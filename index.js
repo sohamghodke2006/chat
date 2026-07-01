001ba4fe82aae85db4411e58bf6f73cdd15f3db0
Soham Ghodke <sohamghodke.2006@gmail.com>
2026-07-01 23:51:58 +0530

    New commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
-app.use("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 main()
@@ -19,6 +20,16 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
 
+// let chat1 = new Chat({
+//     from: "neha",
+//     to: "priya",
+//     msg: "send me notes",
+//     created_at: new Date()
+// });
+// chat1.save()
+//     .then((res) => {
+//         console.log(res);
+//     });
 
 app.get("/", (req, res) => {
     res.send("Working");
