2b6ef37f02cb050651a2edff85987d4600b10219
Soham Ghodke <sohamghodke.2006@gmail.com>
2026-07-01 23:43:53 +0530

    New commit

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const path = require("path");
+
+app.use("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 main()
     .then(() => {
         console.log("Connection Successful");
     })
-    .catch(err => console.log(err));
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
