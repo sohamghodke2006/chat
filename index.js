ee98286365c69104cea727ab5bccc49d2ed2e13c
Soham Ghodke <sohamghodke.2006@gmail.com>
2026-07-02 19:09:12 +0530

    New commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3000;
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -11,6 +12,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(methodOverride("_method"));
 
 main()
     .then(() => {
@@ -31,7 +34,7 @@ app.get("/", (req, res) => {
 // Index Route
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("index.ejs", {chats});
 });
 
@@ -54,6 +57,20 @@ app.post("/chats", (req, res) => {
         .catch((err) => {
             console.log(err);
         });
+    res.redirect("/chats");
+});
+
+// Edit-Update Route
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
+});
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, {msg: newMsg}, {runValidators: true, new: true});
+    console.log(updatedChat);
     res.redirect("/chats");
 });
 
