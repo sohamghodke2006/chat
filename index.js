0575f7fc2bc42acad388a2da98eb2393a8f346aa
Soham Ghodke <sohamghodke.2006@gmail.com>
2026-07-02 19:28:37 +0530

    New commit

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ app.put("/chats/:id", async (req, res) => {
     res.redirect("/chats");
 });
 
+// Delete Route
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
